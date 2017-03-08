e2b54af782ec5cb35623ee4d8c48a2c37a02329e
马家俊 <921711730@qq.com>
2017-03-08 14:26:50 +0800

    新建项目

---
+++ MJButton/MJButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MJButton
+//
+//  Created by 马家俊 on 17/3/8.
+//  Copyright © 2017年 MJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MJButton/MJButton/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MJButton
+//
+//  Created by 马家俊 on 17/3/8.
+//  Copyright © 2017年 MJJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
